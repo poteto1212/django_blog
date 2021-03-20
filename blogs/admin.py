4293bb12e2cd325fb71792f68bac6d06662ef895
Your Name <you@example.com>
2021-03-20 04:58:21 +0000

    init

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Blog
 # Register your models here.
+
+#list_display 一覧表示する際の表示内容
+#list_display_links 一覧表示した際の詳細へのリンク指定
+class BlogAdmin(admin.ModelAdmin):
+    list_display=('id','title','created_datetime','updated_datetime')
+    list_display_links=('id','title')
+
+
+#管理サイトにモデルの操作権限を与えている
+admin.site.register(Blog,BlogAdmin)
+
+#インスタンスとはモデルクラスから生成された個々のオブジェクトを指す。
+#基本的に一つのインスタンスにつき一つのIDが割り振られる
--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+#Blogクラスを生成することでBlogs(複数形)のテーブルが自動生成される。
+#引数(blamk=False 空欄禁止)　引数(null=Falseデータベースの空欄状態もダメ)
+
+class Blog(models.Model):
+    title=models.CharField(blank=False,null=False,max_length=150)
+    text=models.TextField(blank=False)
+    created_datetime=models.DateTimeField(auto_now_add=True)
+    updated_datetime=models.DateTimeField(auto_now=True)
+    
+    #モデルを表示する時の代表的な値を指定
+    #今回titleを指定しているのでBlogモデルを一覧表示した時はtitlカラム内の値で
+    #表示される。
+    def __str__(self):
+        return self.title
+        
+        
